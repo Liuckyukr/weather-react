60726d754bcac5354503ed0b6453970c8eced7b7
Liuckyukr <leroyliudmyla@gmail.com>
2023-04-01 22:45:08 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
-import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
    const [loaded, setLoaded] = useState(false);
